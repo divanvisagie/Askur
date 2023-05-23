7424096aace4d4b9a915b513fe440df551361a2e
Divan Visagie <me@divanv.com>
2023-05-23 22:23:22 +0200

    More swag

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,91 @@
-use std::env;
+use std::{
+    env,
+    sync::{Arc, Mutex},
+};
 
 use actix_web::{App, HttpResponse, HttpServer, Responder};
 use paperclip::actix::{
     api_v2_operation,
     web::{self},
-    OpenApiExt,
+    Apiv2Schema, OpenApiExt,
 };
+use serde::{Deserialize, Serialize};
 
 #[api_v2_operation]
 async fn health() -> impl Responder {
     HttpResponse::Ok().body("All is good")
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize, Apiv2Schema)]
+struct Capability {
+    name: String,
+    description: String,
+    url: String,
+}
+
+#[derive(Clone)]
+struct Registry {
+    capabilities: Vec<Capability>,
+}
+
+impl Registry {
+    fn new() -> Self {
+        Registry {
+            capabilities: Vec::new(),
+        }
+    }
+
+    fn register(&mut self, capability: Capability) {
+        self.capabilities.push(capability);
+    }
+
+    #[allow(dead_code)]
+    fn get(&self, name: String) -> Option<Capability> {
+        for capability in &self.capabilities {
+            if capability.name == name {
+                return Some(capability.clone());
+            }
+        }
+        None
+    }
+}
+
+#[api_v2_operation(
+    tags("Capabilities"),
+    summary = "Register a new capability",
+    description = "Register a new capability with the service registry"
+)]
+fn register_capability(
+    cap: web::Json<Capability>,
+    registry: web::Data<Arc<Mutex<Registry>>>,
+) -> impl Responder {
+    let capability = cap.into_inner();
+    log::info!("Registering capability: {:?}", capability.clone());
+    registry.lock().unwrap().register(capability.clone());
+
+    HttpResponse::Ok().json(capability)
+}
+
+#[api_v2_operation(
+    tags("Capabilities"),
+    summary = "List all capabilities",
+    description = "List all capabilities registered with the service registry"
+)]
+fn list_capabilities(registry: web::Data<Arc<Mutex<Registry>>>) -> impl Responder {
+    let registry = registry.lock().unwrap();
+    HttpResponse::Ok().json(registry.capabilities.clone())
+}
+
+#[api_v2_operation]
+fn get_capability(
+    name: web::Path<String>,
+    registry: web::Data<Arc<Mutex<Registry>>>,
+) -> impl Responder {
+    let registry = registry.lock().unwrap();
+    match registry.get(name.into_inner()) {
+        Some(capability) => HttpResponse::Ok().json(capability),
+        None => HttpResponse::NotFound().body("Capability not found"),
+    }
 }
 
 pub async fn start_server() {
@@ -24,10 +100,18 @@ pub async fn start_server() {
         Err(_) => "127.0.0.1".to_string(),
     };
 
-    let _h = HttpServer::new(|| {
+    let registry = Arc::new(Mutex::new(Registry::new()));
+
+    let _h = HttpServer::new(move || {
         App::new()
             .wrap_api()
+            .app_data(web::Data::new(registry.clone()))
             .service(web::resource("/health").route(web::get().to(health)))
+            .service(
+                web::resource("/capabilities")
+                    .route(web::get().to(list_capabilities))
+                    .route(web::post().to(register_capability)),
+            )
             .with_json_spec_at("/api/spec")
             .with_swagger_ui_at("/swag")
             .build()
